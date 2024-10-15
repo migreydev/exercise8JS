7f89226ee7aeca225804e7842a0818b5950b8a59
Miguel Ángel Reyes Méndez <49988347+migreydev@users.noreply.github.com>
2024-10-15 16:46:39 +0200

    terminando punto 1

--- js/app.js
+++ js/app.js
@@ -31,35 +31,64 @@ agregarCarrito.addEventListener("click", (event) => {
   }
 });
 
-carrito.addEventListener("click", (event) => {
+carrito.addEventListener("mouseenter", (event) => {
   event.preventDefault();
 
+  //Iteramos sobre el array creado anteriormente y que contendra los productos seleccionados
   for (let i = 0; i < arrayCarro.length; i++) {
     let producto = arrayCarro[i];
 
-    let img = producto.querySelector("img");
-    let nombre = producto.querySelector("h4").innerText;
-    let precio = producto.querySelector(".precio").innerText;
+    let img = producto.querySelector(".imagen-curso.u-full-width"); //Toma la etiqueta img del producto
+    let nombre = producto.querySelector("h4").innerText; //Toma el texto que contiene el titulo del producto
+    let precio = producto.querySelector(".precio").innerText; //Toma el precio del producto
+    let cantidad = "";
 
+    //Si el producto es igual al propio producto
+    if (producto === producto) {
+      cantidad++; //Sumamos una cantidad mas
+    }
+
+    //Creamos un elemento i
+    let deleteProduct = document.createElement("button");
+    deleteProduct.className = "borrar-curso"; //Le asociamos la clase
+    deleteProduct.removeChild;
+
+    //Tomamos la tabla del carrito
     let tablaCarro = document.querySelector("#lista-carrito");
 
+    //Tomamos el body de la tabla del carrito
     let tbody = tablaCarro.querySelector("tbody");
 
+    //Creamos un elemento fila
     let fila = document.createElement("tr");
 
+    //Creamos un elemento celda para la imagen
     let celdaImg = document.createElement("td");
-    celdaImg.appendChild(img);
+    celdaImg.appendChild(img); //Añadimos el hijo a la celda imagen
 
+    //Creamos un elemento celda del nombre
     let celdaNombre = document.createElement("td");
-    celdaNombre.innerText = nombre;
+    celdaNombre.innerText = nombre; //Añadimos el nombre del producto a la celda
 
+    //Creamos un elemento celda para el precio
     let celdaPrecio = document.createElement("td");
-    celdaPrecio.innerText = precio;
+    celdaPrecio.innerText = precio; //Añadimos el valor del precio a la celda
 
+    //Creamos un elemento celda para la cantidad
+    let celdaCantidad = document.createElement("td");
+    celdaCantidad.innerText = cantidad; //Añadimos el valor de la cantidad a la celda
+
+    let celdaRemove = document.createElement("td");
+    celdaRemove.appendChild(deleteProduct);
+
+    //Añadimos las celdas al elemento fila
     fila.appendChild(celdaImg);
     fila.appendChild(celdaNombre);
     fila.appendChild(celdaPrecio);
+    fila.appendChild(celdaCantidad);
+    fila.appendChild(celdaRemove);
 
+    //Añadimos la fila al cuerpo de la tabla
     tbody.appendChild(fila);
   }
 });
